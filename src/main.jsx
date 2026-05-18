8cf9bf392cd686fcaf05668be69a93d73ea325dd
zsokica002 <sofija.curcic@gmail.com>
2026-05-18 10:45:57 +0200

    male promene, dodat sass?

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import "./sass/styles.scss"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  <App />
+  // </StrictMode>,
 )
