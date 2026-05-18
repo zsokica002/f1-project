355b2e7e00362cef53a4c18fa6199f2df4c493f0
zsokica002 <sofija.curcic@gmail.com>
2026-05-18 13:22:01 +0200

    dodat races na nav

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
       <ul>
         <li><Link to="/" >Home</Link></li>
         <li><Link to="/teams" >Teams</Link></li>
-        <li><Link to="/races"></Link></li>
+        <li><Link to="/races">Races</Link></li>
       </ul>
 
       <Routes>
