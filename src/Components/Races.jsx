77f850f47f825c696c18c6d3964a16496d32aeef
isakusha <isakusha@gmail.com>
2026-05-18 15:51:15 +0200

    dodat race

--- src/Components/Races.jsx
+++ src/Components/Races.jsx
@@ -1,20 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loader from "./Loader";
+import axios from "axios";
 
-export default function App() {
-    const [allRaces, setAllRaces] = useState();
-    const [loading, setLoading] = useState();
+export default function Races() {
+    const [races, setRaces] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        getAllRaces();
+        getRaces();
 
     }, []);
 
-    const getAllRaces = async () => {
-        const urs = "https://api.jolpi.ca/ergast/f1/2025/results/1.json";
+    const getRaces = async () => {
+        const url = "https://api.jolpi.ca/ergast/f1/2025/results/1.json";
 
         const response = await axios.get(url);
-        setAllRaces(response.data);
+        console.log(response.data.MRData.RaceTable.Races);
+
+        setRaces(response.data.MRData.RaceTable.Races);
         setLoading(false);
     }
 
@@ -24,7 +27,35 @@ export default function App() {
 
 
     return (
-        <>
-        </>
+        <table>
+            <thead>
+                <tr>
+                    <td>Race calendar 2025</td>
+                </tr>
+                <tr>
+                    <th>Round</th>
+                    <th>Grand Prix</th>
+                    <th>Circuit</th>
+                    <th>Date</th>
+                    <th>Winner</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {races.map((race, i) => {
+                    return (
+                        <tr key={i}>
+                            <td>{race.round}</td>
+                            <td>{race.raceName} </td>
+                            <td>{race.Circuit.circuitName}</td>
+                            <td>{race.date}</td>
+                            <td>{race.Results[0].Driver.familyName}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+
+
+        </table >
     )
 }
