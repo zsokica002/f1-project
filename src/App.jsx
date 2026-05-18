a50611c1fdda13421e627148016e79e6affb51bc
zsokica002 <sofija.curcic@gmail.com>
2026-05-18 11:46:10 +0200

    sass provera, font: arial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Loader from "./Components/Loader";
+import { BrowserRouter, Routes, Route, Link } from "react-router";
 import Drivers from "./components/Drivers";
 
 export default function App() {
