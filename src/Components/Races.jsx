64bbac98792adad2febb1686642aecb81bc5d005
isakusha <isakusha@gmail.com>
2026-05-18 12:47:53 +0200

    ubacen races u App

--- src/Components/Races.jsx
+++ src/Components/Races.jsx
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
 import Loader from "./Components/Loader";
 
 export default function App() {
-    const [allRaces, useAllRaces] = useState();
+    const [allRaces, setAllRaces] = useState();
     const [loading, setLoading] = useState();
+
+    useEffect(() => {
+        getAllRaces();
+
+    }, []);
+
+    const getAllRaces = async () => {
+        const urs = "https://api.jolpi.ca/ergast/f1/2025/results/1.json";
+
+        const response = await axios.get(url);
+        setAllRaces(response.data);
+        setLoading(false);
+    }
+
+    if (loading) {
+        return <Loader />
+    }
 
 
     return (
