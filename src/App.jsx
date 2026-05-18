d4e5b0213629b56ca0a11e4e76bcceac6caf515d
zsokica002 <sofija.curcic@gmail.com>
2026-05-18 12:52:24 +0200

    greska ispravljena

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Drivers />} />
-        <Route path="/races" element={<Races />}
+        <Route path="/races" element={<Races />} />
       </Routes>
 
       <AllTeams />
