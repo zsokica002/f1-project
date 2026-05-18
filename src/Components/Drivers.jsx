7c9261eaee2ef420100595e87e5441f9f3064604
ivaKonc <10.3.1.konc@gmail.com>
2026-05-18 12:56:36 +0200

    promene u Drivers.jsx

--- src/Components/Drivers.jsx
+++ src/Components/Drivers.jsx
@@ -1,29 +1,36 @@
 import { useEffect, useState } from "react";
-import axios from axios;
+import axios from "axios";
+
 
 export default function Drivers() {
 
-    const [drivers, setDrivers] = useState([]);
-    const [loadin, setLoading] = useState(true);
-    useEffect(() => {
-        getDrivers();
-    }, []);
+  const [drivers, setDrivers] = useState([]);
 
-    const getDrivers = async () => {
-        const url = "https://api.jolpi.ca/ergast/f1/2025/driverStandings.json"
-        const results = axios.get(url);
-        setDrivers(results.data);
-        console.log(results.data);
-        setLoading(false);
-    }
 
-    if (loadin) {
-        return <Loader />
-    }
+  useEffect(() => {
+    getDrivers();
+  }, []);
 
-    return (
-        <>
-        <h1>HELLo tHERE!</h1>
-        </>
-    );
+  const getDrivers = async () => {
+    const url = "https://api.jolpi.ca/ergast/f1/2025/driverStandings.json"
+    const response = await axios.get(url);
+    setDrivers(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    console.log(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+
+  }
+
+  return (
+    <>
+      <h1>HELLo tHERE!</h1>
+
+      {drivers.map((driver) => {
+        return (
+            <div key={driver.Driver.permanentNumber}>
+              {/* OVO JE SAMO PRIVREMENO DA PROVERIM DA LI MI DOBRO RADI MAP */}
+              <p>{driver.Driver.code}</p>
+            </div>
+        );
+      })}
+    </>
+  );
 }
--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,4 +1,4 @@
-import { MoonLoader } from "react-spinners"
+import MoonLoader  from "react-spinners"
 export default function Loader() {
 
     return (
