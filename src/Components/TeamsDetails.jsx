8c31296b74b257a9b02622ea2c9af2604c226d62
milancubrilo50 <milancubrilo50@gmail.com>
2026-05-19 11:54:44 +0200

    19051154

--- src/Components/TeamsDetails.jsx
+++ src/Components/TeamsDetails.jsx
@@ -3,11 +3,34 @@ import Loader from "./Loader";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router";
 
+
+
 export default function TeamsDetails() {
+    
+    // const [teamsResults,setTeamsResults] = useState(null)
+    
+    
+    
     const [teamsDetails, setTeamsDetails] = useState(null);
     const [loading, setLoading] = useState(true);
     const params = useParams();
     const navigate = useNavigate();
+
+
+    // useEffect(() => {
+    //     getTeamsresults();
+    // }, [])
+
+    // const getTeamsResults = async () => {
+    //     const url = `https://api.jolpi.ca/ergast/f1/2025/constructors/${params.id}/results.json`;
+
+
+    //     const response = await axios.get(url);
+    //     console.log(response.data.MRData.total);
+    //     setTeamsResults(response.data);
+    //     setLoading(false);
+    // };
+
 
     useEffect(() => {
         getTeamsDetails();
@@ -32,6 +55,13 @@ export default function TeamsDetails() {
 
     return (
         <>
+            <div>
+                
+            {/* <p>total:{response.data.MRData.total} </p> */}
+
+            </div>
+
+
             <p>Teams details</p>
             <a target="_blank" href={teamsDetails.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0].Constructor.url}>wiki</a>
 
