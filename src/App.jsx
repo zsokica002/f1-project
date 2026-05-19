6cd2ebdcf4eebfe80cf43fd07a26c41ccd95eb3e
ivaKonc <10.3.1.konc@gmail.com>
2026-05-19 12:55:42 +0200

    zastave

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,23 @@ import AllTeams from "./components/AllTeams";
 import Races from "./components/Races";
 import TeamsDetails from "./components/TeamsDetails";
 import Qualifiers from "./components/Qualifiers";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 export default function App() {
+  const [flags, setFlags] = useState([]);
+
+  useEffect(() => {
+    getFlags();
+  }, []);
+
+  const getFlags = async () => {
+    const url = "https://raw.githubusercontent.com/Imagin-io/country-nationality-list/refs/heads/master/countries.json";
+    const response = await axios.get(url);
+    setFlags(response.data);
+    console.log(response.data);
+  }
 
   return (
     <BrowserRouter>
@@ -17,7 +31,7 @@ export default function App() {
       </ul>
 
       <Routes>
-        <Route path="/" element={<Drivers />} />
+        <Route path="/" element={<Drivers flags = {flags}/>} />
         <Route path="/teams" element={<AllTeams />} />
         <Route path="/races" element={<Races />} />
         <Route path="/teamsDetails/:id" element={<TeamsDetails />} />
--- src/Components/DriverDetails.jsx
+++ src/Components/DriverDetails.jsx
@@ -1,6 +1,13 @@
-export default function DriverDetails(){
-    return(
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Loader from "./Loader";
+import axios from "axios";
+
+export default function DriverDetails() {
+
+    return (
         <>
+            <h1>Driver Details</h1>
         </>
     );
 }
