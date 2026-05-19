6b20182a1bd4eedf498bc20191aa34581e581279
vulicns1 <vulicborislava@gmail.com>
2026-05-19 14:38:36 +0200

    url dodala Sofija

--- src/components/AllTeams.jsx
+++ src/components/AllTeams.jsx
@@ -51,9 +51,12 @@ export default function AllTeams() {
                         return (
                             <tr key={team.Constructor.constructorId}>
                                 <td>{team.position}</td>
-                                <td>{team.Constructor.name}</td>
-                                <td> <input type="button" value="Details" className="btn"
-                                    onClick={() => handleClickDetails(team.Constructor.constructorId)} /></td>
+                                <td onClick={() => handleClickDetails(team.Constructor.constructorId)}
+                                > {team.Constructor.name}</td>
+                                <td><a target="_blank" href={team.Constructor.url}>Details</a></td>
+                               
+                              
+                                
                                 <td>{team.points}</td>
                                 {/*<td>{team.wins}</td>
                                 <td>{team.Constructor.nationality}</td>*/}
