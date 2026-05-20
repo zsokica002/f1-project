339b5456d7f0bd5021542082fa96dfac01e06ca9
zsokica002 <sofija.curcic@gmail.com>
2026-05-20 09:34:39 +0200

    neki merge

--- src/components/AllTeams.jsx
+++ src/components/AllTeams.jsx
@@ -59,12 +59,7 @@ export default function AllTeams() {
                                 > {team.Constructor.name}</td>
                                 <td><a target="_blank" href={team.Constructor.url}>Details</a></td>
 
-
-
                                 <td>{team.points}</td>
-                                {/*<td>{team.wins}</td>
-                                <td>{team.Constructor.nationality}</td>*/}
-
                             </tr>
                         );
                     })}
