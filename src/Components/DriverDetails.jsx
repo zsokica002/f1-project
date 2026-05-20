cb659e212751ce19007d100cbf7461f942a5e288
zsokica002 <sofija.curcic@gmail.com>
2026-05-20 10:58:11 +0200

    dodato jos ruta

--- src/Components/DriverDetails.jsx
+++ src/Components/DriverDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import Loader from "./Loader";
 import axios from "axios";
 import Flag from "react-flagkit";
@@ -11,41 +11,50 @@ export default function DriverDetails(props) {
     const [loading, setLoading] = useState(true);
 
     const params = useParams();
+    const navigate = useNavigate();
 
-    
 
-    useEffect(()=>{
+    useEffect(() => {
         getDriverDetails();
-    },[]);
+    }, []);
 
-    const getDriverDetails = async()=>{
-       const urlDriverInfo = `https://api.jolpi.ca/ergast/f1/2025/drivers/${params.id}/driverStandings.json`;
-       const urlDriverResults = `https://api.jolpi.ca/ergast/f1/2025/drivers/${params.id}/results.json`;
+    const getDriverDetails = async () => {
+        const urlDriverInfo = `https://api.jolpi.ca/ergast/f1/2025/drivers/${params.id}/driverStandings.json`;
+        const urlDriverResults = `https://api.jolpi.ca/ergast/f1/2025/drivers/${params.id}/results.json`;
 
-       const response1 = await axios.get(urlDriverInfo);
-       const response2 = await axios.get(urlDriverResults);
+        const response1 = await axios.get(urlDriverInfo);
+        const response2 = await axios.get(urlDriverResults);
 
-       console.log(response1.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]);
-       console.log(response2.data.MRData.RaceTable.Races);
-       setDriverInfo(response1.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]);
-       setDriverResults(response2.data.MRData.RaceTable.Races);
+        // console.log(response1.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]);
+        // console.log(response2.data.MRData.RaceTable.Races);
+        setDriverInfo(response1.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]);
+        setDriverResults(response2.data.MRData.RaceTable.Races);
 
-       setLoading(false);
+        setLoading(false);
+
     }
 
-    const getDriverNationality = (nation)=>{
-       const driverFlagA2 = props.flags.find(flag => flag.nationality === nation);
-    return driverFlagA2?.alpha_2_code;
+    const getDriverNationality = (nation) => {
+        const driverFlagA2 = props.flags.find(flag => flag.nationality === nation);
+        return driverFlagA2?.alpha_2_code;
     }
 
-    const getRaceLocation = (nation)=> {
+    const getRaceLocation = (nation) => {
         const raceFlagA2 = props.flags.find(flag => flag.en_short_name === nation)
         return raceFlagA2?.alpha_2_code;
     }
 
-    if(loading){
-        return <Loader/>
+    const handleClickDetails = (id) => {
+        navigate(`/teamDetails/${id}`);
+    };
+
+
+    if (loading) {
+        return <Loader />
     }
+
+    console.log(driverResults);
+
 
     return (
         <>
@@ -53,13 +62,14 @@ export default function DriverDetails(props) {
 
             <div>
                 <div>
-                    <img src = {`/public/drivers2025/${driverInfo.Driver.driverId}.jpg`} alt={driverInfo.Driver.driverId}  width={250}/>
+                    <img src={`/public/drivers2025/${driverInfo.Driver.driverId}.jpg`} alt={driverInfo.Driver.driverId} width={250} />
                     <div>
                         <p>{driverInfo.Driver.givenName} {driverInfo.Driver.familyName}</p>
-                        <div> <Flag country={getDriverNationality(driverInfo.Driver.nationality)} size={50}/></div>    
-                    </div>    
+                        <div> <Flag country={getDriverNationality(driverInfo.Driver.nationality)} size={50} /></div>
+                    </div>
                     <p><b>Country: </b>{driverInfo.Driver.nationality}</p>
-                    <p><b>Team: </b>{driverInfo.Constructors[0].name}</p>
+                    <p onClick={() => handleClickDetails(driverInfo.Constructors[0].constructorId)}
+                    ><b>Team: </b>{driverInfo.Constructors[0].name}</p>
                     <p><b>Birth: </b>{driverInfo.Driver.dateOfBirth}</p>
                     <p><a href={driverInfo.Driver.url} target="_blank"><b>Biography</b></a></p>
                 </div>
@@ -73,19 +83,20 @@ export default function DriverDetails(props) {
                         <th>Team</th>
                         <th>Grid</th>
                         <th>Position</th>
-                        </tr>
+                    </tr>
                 </thead>
                 <tbody>
-                    {driverResults.map((result, i)=>{
-                       return(
-                         <tr key={i}>
-                        <td>{result.Circuit.circuitName}</td>
-                        <td><Flag country={getRaceLocation(result.Circuit.Location.country)}/>{result.Circuit.Location.country}</td>
-                        <td>{result.Results[0].Constructor.name}</td>
-                        <td>{result.Results[0].grid}</td>
-                        <td>{result.Results[0].position}</td>
-                    </tr>
-                       );
+                    {driverResults.map((result, i) => {
+                        return (
+                            <tr key={i}>
+                                <td><a target="_blank" href={result.Circuit.url}>{result.Circuit.circuitName}</a></td>
+                                <td><Flag country={getRaceLocation(result.Circuit.Location.country)} />{result.Circuit.Location.country}</td>
+                                <td onClick={() => handleClickDetails(result.Results[0].Constructor.constructorId)}
+                                >{result.Results[0].Constructor.name}</td>
+                                <td>{result.Results[0].grid}</td>
+                                <td>{result.Results[0].position}</td>
+                            </tr>
+                        );
                     })}
                 </tbody>
             </table>
