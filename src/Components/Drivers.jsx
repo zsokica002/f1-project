81e29e71485db69459afc7fd24a27f72621729f6
ivaKonc <10.3.1.konc@gmail.com>
2026-05-20 12:13:48 +0200

    fix: adjust table border

--- src/Components/Drivers.jsx
+++ src/Components/Drivers.jsx
@@ -43,9 +43,10 @@ export default function Drivers(props) {
 
   return (
     <>
-    <Breadcrumbs/>
+      <Breadcrumbs />
       <h1>Drivers Championship</h1>
       <table className="table" border={1}>
+      <table className="table" >
         <thead>
           <tr>
             <th colSpan={4}>Drivers Championship Standings - {year}</th>
