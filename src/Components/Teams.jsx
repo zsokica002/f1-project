50788c8d2b0a4db1430a92ca3b9af7f59a3f3ae5
vulicns1 <vulicborislava@gmail.com>
2026-05-22 09:29:12 +0200

    className table

--- src/Components/Teams.jsx
+++ src/Components/Teams.jsx
@@ -69,7 +69,7 @@ export default function AllTeams(props) {
         <>
             <Breadcrumbs items={breadcrumbs} />
             <h2>All Teams 2025</h2>
-            <table className="container border" border={1} >
+            <table className="table" border={1} >
                 <thead>
                     <tr>
                         <th colSpan={3}>Constructors Championship Standings - {year}</th>
