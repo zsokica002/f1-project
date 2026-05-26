ea3bde703e23cf0613868e3e6805d1d1f0d8afe1
zsokica002 <sofija.curcic@gmail.com>
2026-05-26 15:31:49 +0200

    izbrisana godina sa home page-a

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,23 +4,19 @@ export default function Home() {
 
     return (
         <div>
-
             <video src="/bg-video.mp4" autoPlay muted loop id="background-video"></video>
 
             <div className="content">
                 <h1>Formula 1 Racing Info Sheets</h1>
 
-                <p>Welcome to the landing page, nerd! Retire your janky Excel spredsheet and make way for this super sleek and sexy website for all your 2025 season F1 needs that you totally can't find anywhere else, only here! Stay informed about all your favorite drivers with our thorough database and organized with our state of the art filter and search super-combo! </p>
+                <p>Welcome to the landing page, nerd! Retire your janky Excel spredsheet and make way for this super sleek and sexy website for all your F1 needs that you totally can't find anywhere else, only here! Stay informed about all your favorite drivers with our thorough database and organized with our state of the art filter and search super-combo! </p>
 
                 <div className="buttons">
                     <Link className="btn" to="/drivers">Drivers</Link>
                     <Link className="btn" to="/teams">Teams</Link>
                     <Link className="btn" to="/races">Races</Link>
                 </div>
-
             </div>
-
-
         </div>
     );
 }
